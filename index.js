9dfbae9a4b2bc22e3eeaf87bb899bf68167081c2
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-04-26 23:23:18 +0600

    get email data to database

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors())
+
+app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xcjib6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -17,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -25,104 +26,50 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const touristsSpotCollection = client.db('touristsSpotDB').collection('touristsSpot')
-    // const userCollection = client.db('newUserDB').collection('users');
+    const touristsSpotCollection = client.db("touristsSpotDB").collection("touristsSpot");
 
-  // Tourists Spot
-    app.get(`/tourists`, async(req, res) => {
-        const cursor = touristsSpotCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    // Tourists Spot
+    app.get(`/tourists`, async (req, res) => {
+      const cursor = touristsSpotCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
     });
 
-    app.get(`/tourists/:id`, async(req, res) => {
+    app.get(`/tourists/:email`, async (req, res) => {
+      const email = req.params.email;
+      const query = {email: email};
+      const cursor =  touristsSpotCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get(`/single-tourists/:id`, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await touristsSpotCollection.findOne(query);
       res.send(result);
     });
 
-    app.post(`/tourists`, async(req, res) => {
-        const coffee = req.body
-        console.log(coffee);
-        const result = await touristsSpotCollection.insertOne(coffee)
-        res.send(result);
-
-    })
-
-    app.put(`/tourists/:id`, async (req, res) => {
-      const id = req.params.id
-      const coffee = req.body
-      const filter = {_id: new ObjectId(id)}
-      const options = {upsert: true}
-      const updateCoffee = {
-        $set: {
-        name : coffee.name,
-        chef : coffee.chef,
-        supplier : coffee.supplier,
-        taste : coffee.taste,
-        category : coffee.category,
-        details : coffee.details,
-        photo : coffee.photo,
-        }
-      } 
-      const result = await touristsSpotCollection.updateOne(filter, updateCoffee, options)
+    app.post(`/tourists`, async (req, res) => {
+      const tourist = req.body;
+      console.log(tourist);
+      const result = await touristsSpotCollection.insertOne(tourist);
       res.send(result);
     });
 
-    app.delete(`/tourists/:id`, async(req, res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
+    app.delete(`/tourists/:id`, async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
       const result = await touristsSpotCollection.deleteOne(query);
       res.send(result);
-    })
-
-
-    // Users
-
-    app.get(`/users`, async(req, res) => {
-      const cursor = userCollection.find()
-      const result = await cursor.toArray();
-      res.send(result);
-    })
-
-    app.get(`/users/:id`, async(req, res) =>{
-      const id = req.params.ids
-      
     });
-    
-    app.post(`/users`, async(req, res) => {
-      const user = req.body
-      console.log(user);
-      const result = await userCollection.insertOne(user);
-      res.send(result);
-    })
-
-    app.patch(`/users`, async(req, res) => {
-      const user = req.body
-      console.log(user);
-      const filter = { email: user.email}
-      const options = { upsert: true };
-      const updateDoc = {
-        $set:{
-          lastLogin : user.lastLogin
-        }
-      }
-      const result = await userCollection.updateOne(filter, updateDoc, options);
-      res.send(result)
-    })
-
-    app.delete(`/users/:id`, async(req, res) => {
-      const id = req.params.id
-      console.log(id);
-      const query = {_id: new ObjectId(id)}
-      const result = await userCollection.deleteOne(query);
-      res.send(result);
-    })
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -130,12 +77,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Master Tourist Server is running");
+});
 
-
-app.get('/', (req, res) => {
-    res.send('Master Tourist Server is running')
-})
-
-app.listen(port, () =>{
-    console.log(`Coffer server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Master server running on port ${port}`);
+});
